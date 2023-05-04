bc12c11fbc0cbd2cd8f02760651f4d7b6613e142
Tsukasa OI <floss_ssdeep@irq.a4lg.com>
2023-05-04 11:59:30 +0000

    tests: Refine tests on crate::test_utils
    We found another test compilation failure on the `no_alloc` environment.
    Along with this fix, this commit (again) reorganizes the test for
    `crate::test_utils`.

--- ffuzzy/src/test_utils.rs
+++ ffuzzy/src/test_utils.rs
@@ -112,11 +112,13 @@ pub(crate) use assert_fits_in_impl as assert_fits_in;
 // grcov-excl-br-start
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn test_auto_clone() {
+    fn test_auto_clone_valid() {
         #[derive(PartialEq, Eq, Clone, Debug)]
         struct Example(u8);
-        super::test_auto_clone(&Example(1));
+        test_auto_clone(&Example(1));
     }
 
     #[test]
@@ -128,111 +130,117 @@ mod tests {
             // BROKEN: returns fixed value rather than itself.
             fn clone(&self) -> Self { Self(0) }
         }
-        super::test_auto_clone(&Counterexample(1));
+        test_auto_clone(&Counterexample(1));
     }
 
-    #[derive(Debug)]
-    enum AutoEnumExample {
-        OK1,
-        OK2,
-        OK3,
-    }
-    enum DebugImplEnumExample {
-        OK1,
-        OK2,
-        OK3,
-        Broken,
-    }
-    impl core::fmt::Debug for DebugImplEnumExample {
-        fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-            match self {
-                // Matches to the auto-generated implementation.
-                Self::OK1 => write!(f, "OK1"),
-                Self::OK2 => write!(f, "OK2"),
-                Self::OK3 => write!(f, "OK3"),
-                // BROKEN: it does not match.
-                // We could test enums with tuples or structs but that would
-                // only make mostly duplicate test cases.
-                Self::Broken => write!(f, "I_SAID_BROKEN"),
+
+    #[cfg(feature = "alloc")]
+    mod test_auto_debug_for_enum {
+        use super::*;
+
+        #[derive(Debug)]
+        enum AutoEnumExample {
+            OK1,
+            OK2,
+            OK3,
+        }
+        enum DebugImplEnumExample {
+            OK1,
+            OK2,
+            OK3,
+            Broken,
+        }
+        impl core::fmt::Debug for DebugImplEnumExample {
+            fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+                match self {
+                    // Matches to the auto-generated implementation.
+                    Self::OK1 => write!(f, "OK1"),
+                    Self::OK2 => write!(f, "OK2"),
+                    Self::OK3 => write!(f, "OK3"),
+                    // BROKEN: it does not match.
+                    // We could test enums with tuples or structs but that would
+                    // only make mostly duplicate test cases.
+                    Self::Broken => write!(f, "I_SAID_BROKEN"),
+                }
             }
         }
-    }
-    #[test]
-    fn test_auto_debug_for_enum() {
-        super::test_auto_debug_for_enum!(AutoEnumExample, []);
-        super::test_auto_debug_for_enum!(AutoEnumExample, [ OK1 ]);
-        super::test_auto_debug_for_enum!(AutoEnumExample, [ OK1, ]);
-        super::test_auto_debug_for_enum!(AutoEnumExample, [ OK1, OK2 ]);
-        super::test_auto_debug_for_enum!(AutoEnumExample, [ OK1, OK2, ]);
-        super::test_auto_debug_for_enum!(AutoEnumExample, [ OK1, OK2, OK3 ]);
-        super::test_auto_debug_for_enum!(AutoEnumExample, [ OK1, OK2, OK3, ]);
-        super::test_auto_debug_for_enum!(DebugImplEnumExample, []);
-        super::test_auto_debug_for_enum!(DebugImplEnumExample, [ OK1 ]);
-        super::test_auto_debug_for_enum!(DebugImplEnumExample, [ OK1, ]);
-        super::test_auto_debug_for_enum!(DebugImplEnumExample, [ OK1, OK2 ]);
-        super::test_auto_debug_for_enum!(DebugImplEnumExample, [ OK1, OK2, ]);
-        super::test_auto_debug_for_enum!(DebugImplEnumExample, [ OK1, OK2, OK3 ]);
-        super::test_auto_debug_for_enum!(DebugImplEnumExample, [ OK1, OK2, OK3, ]);
-    }
-    #[test]
-    #[should_panic]
-    fn test_auto_debug_for_enum_counterexample_01() {
-        super::test_auto_debug_for_enum!(DebugImplEnumExample, [ Broken ]);
-    }
-    #[test]
-    #[should_panic]
-    fn test_auto_debug_for_enum_counterexample_02() {
-        super::test_auto_debug_for_enum!(DebugImplEnumExample, [ Broken, ]);
-    }
-    #[test]
-    #[should_panic]
-    fn test_auto_debug_for_enum_counterexample_03() {
-        super::test_auto_debug_for_enum!(DebugImplEnumExample, [ Broken, OK1 ]);
-    }
-    #[test]
-    #[should_panic]
-    fn test_auto_debug_for_enum_counterexample_04() {
-        super::test_auto_debug_for_enum!(DebugImplEnumExample, [ Broken, OK1, ]);
-    }
-    #[test]
-    #[should_panic]
-    fn test_auto_debug_for_enum_counterexample_05() {
-        super::test_auto_debug_for_enum!(DebugImplEnumExample, [ OK1, Broken ]);
-    }
-    #[test]
-    #[should_panic]
-    fn test_auto_debug_for_enum_counterexample_06() {
-        super::test_auto_debug_for_enum!(DebugImplEnumExample, [ OK1, Broken, ]);
-    }
-    #[test]
-    #[should_panic]
-    fn test_auto_debug_for_enum_counterexample_07() {
-        super::test_auto_debug_for_enum!(DebugImplEnumExample, [ Broken, OK1, OK2 ]);
-    }
-    #[test]
-    #[should_panic]
-    fn test_auto_debug_for_enum_counterexample_08() {
-        super::test_auto_debug_for_enum!(DebugImplEnumExample, [ Broken, OK1, OK2, ]);
-    }
-    #[test]
-    #[should_panic]
-    fn test_auto_debug_for_enum_counterexample_09() {
-        super::test_auto_debug_for_enum!(DebugImplEnumExample, [ OK1, Broken, OK2 ]);
-    }
-    #[test]
-    #[should_panic]
-    fn test_auto_debug_for_enum_counterexample_10() {
-        super::test_auto_debug_for_enum!(DebugImplEnumExample, [ OK1, Broken, OK2, ]);
-    }
-    #[test]
-    #[should_panic]
-    fn test_auto_debug_for_enum_counterexample_11() {
-        super::test_auto_debug_for_enum!(DebugImplEnumExample, [ OK1, OK2, Broken ]);
-    }
-    #[test]
-    #[should_panic]
-    fn test_auto_debug_for_enum_counterexample_12() {
-        super::test_auto_debug_for_enum!(DebugImplEnumExample, [ OK1, OK2, Broken, ]);
+        #[test]
+        fn test_valid() {
+            test_auto_debug_for_enum!(AutoEnumExample, []);
+            test_auto_debug_for_enum!(AutoEnumExample, [ OK1 ]);
+            test_auto_debug_for_enum!(AutoEnumExample, [ OK1, ]);
+            test_auto_debug_for_enum!(AutoEnumExample, [ OK1, OK2 ]);
+            test_auto_debug_for_enum!(AutoEnumExample, [ OK1, OK2, ]);
+            test_auto_debug_for_enum!(AutoEnumExample, [ OK1, OK2, OK3 ]);
+            test_auto_debug_for_enum!(AutoEnumExample, [ OK1, OK2, OK3, ]);
+            test_auto_debug_for_enum!(DebugImplEnumExample, []);
+            test_auto_debug_for_enum!(DebugImplEnumExample, [ OK1 ]);
+            test_auto_debug_for_enum!(DebugImplEnumExample, [ OK1, ]);
+            test_auto_debug_for_enum!(DebugImplEnumExample, [ OK1, OK2 ]);
+            test_auto_debug_for_enum!(DebugImplEnumExample, [ OK1, OK2, ]);
+            test_auto_debug_for_enum!(DebugImplEnumExample, [ OK1, OK2, OK3 ]);
+            test_auto_debug_for_enum!(DebugImplEnumExample, [ OK1, OK2, OK3, ]);
+        }
+        #[test]
+        #[should_panic]
+        fn test_counterexample_01() {
+            test_auto_debug_for_enum!(DebugImplEnumExample, [ Broken ]);
+        }
+        #[test]
+        #[should_panic]
+        fn test_counterexample_02() {
+            test_auto_debug_for_enum!(DebugImplEnumExample, [ Broken, ]);
+        }
+        #[test]
+        #[should_panic]
+        fn test_counterexample_03() {
+            test_auto_debug_for_enum!(DebugImplEnumExample, [ Broken, OK1 ]);
+        }
+        #[test]
+        #[should_panic]
+        fn test_counterexample_04() {
+            test_auto_debug_for_enum!(DebugImplEnumExample, [ Broken, OK1, ]);
+        }
+        #[test]
+        #[should_panic]
+        fn test_counterexample_05() {
+            test_auto_debug_for_enum!(DebugImplEnumExample, [ OK1, Broken ]);
+        }
+        #[test]
+        #[should_panic]
+        fn test_counterexample_06() {
+            test_auto_debug_for_enum!(DebugImplEnumExample, [ OK1, Broken, ]);
+        }
+        #[test]
+        #[should_panic]
+        fn test_counterexample_07() {
+            test_auto_debug_for_enum!(DebugImplEnumExample, [ Broken, OK1, OK2 ]);
+        }
+        #[test]
+        #[should_panic]
+        fn test_counterexample_08() {
+            test_auto_debug_for_enum!(DebugImplEnumExample, [ Broken, OK1, OK2, ]);
+        }
+        #[test]
+        #[should_panic]
+        fn test_counterexample_09() {
+            test_auto_debug_for_enum!(DebugImplEnumExample, [ OK1, Broken, OK2 ]);
+        }
+        #[test]
+        #[should_panic]
+        fn test_counterexample_10() {
+            test_auto_debug_for_enum!(DebugImplEnumExample, [ OK1, Broken, OK2, ]);
+        }
+        #[test]
+        #[should_panic]
+        fn test_counterexample_11() {
+            test_auto_debug_for_enum!(DebugImplEnumExample, [ OK1, OK2, Broken ]);
+        }
+        #[test]
+        #[should_panic]
+        fn test_counterexample_12() {
+            test_auto_debug_for_enum!(DebugImplEnumExample, [ OK1, OK2, Broken, ]);
+        }
     }
 
 
@@ -246,7 +254,7 @@ mod tests {
         impl Default for Example {
             fn default() -> Self { Self::new() }
         }
-        super::test_recommended_default!(Example);
+        test_recommended_default!(Example);
     }
 
     #[test]
@@ -261,130 +269,134 @@ mod tests {
             // BROKEN: intentionally different from the result of `new()`.
             fn default() -> Self { Self(1) }
         }
-        super::test_recommended_default!(Counterexample);
+        test_recommended_default!(Counterexample);
     }
 
 
-    struct TestTargetType<const IS_OK: bool>;
-    impl<const IS_OK: bool> TestTargetType<IS_OK> {
-        const IS_OK: bool = IS_OK;
-    }
-    type OKType = TestTargetType<true>;
-    type NGType = TestTargetType<false>;
-    macro_rules! test {
-        ($ty: ty) => {
-            // Passing `NGType` will cause an assertion failure.
-            assert!(<$ty>::IS_OK);
-        };
-    }
+    mod test_for_each_type {
+        use super::*;
 
-    #[test]
-    fn test_for_each_type() {
-        super::test_for_each_type!(test, []);
-        super::test_for_each_type!(test, [ OKType ]);
-        super::test_for_each_type!(test, [ OKType, ]);
-        super::test_for_each_type!(test, [ OKType, OKType ]);
-        super::test_for_each_type!(test, [ OKType, OKType, ]);
-        super::test_for_each_type!(test, [ OKType, OKType, OKType ]);
-        super::test_for_each_type!(test, [ OKType, OKType, OKType, ]);
-    }
-    #[test]
-    #[should_panic]
-    fn test_for_each_type_counterexample_01() {
-        super::test_for_each_type!(test, [ NGType ]);
-    }
-    #[test]
-    #[should_panic]
-    fn test_for_each_type_counterexample_02() {
-        super::test_for_each_type!(test, [ NGType, ]);
-    }
-    #[test]
-    #[should_panic]
-    fn test_for_each_type_counterexample_03() {
-        super::test_for_each_type!(test, [ NGType, OKType ]);
-    }
-    #[test]
-    #[should_panic]
-    fn test_for_each_type_counterexample_04() {
-        super::test_for_each_type!(test, [ NGType, OKType, ]);
-    }
-    #[test]
-    #[should_panic]
-    fn test_for_each_type_counterexample_05() {
-        super::test_for_each_type!(test, [ OKType, NGType ]);
-    }
-    #[test]
-    #[should_panic]
-    fn test_for_each_type_counterexample_06() {
-        super::test_for_each_type!(test, [ OKType, NGType, ]);
-    }
-    #[test]
-    #[should_panic]
-    fn test_for_each_type_counterexample_07() {
-        super::test_for_each_type!(test, [ NGType, OKType, OKType ]);
-    }
-    #[test]
-    #[should_panic]
-    fn test_for_each_type_counterexample_08() {
-        super::test_for_each_type!(test, [ NGType, OKType, OKType, ]);
-    }
-    #[test]
-    #[should_panic]
-    fn test_for_each_type_counterexample_09() {
-        super::test_for_each_type!(test, [ OKType, NGType, OKType ]);
-    }
-    #[test]
-    #[should_panic]
-    fn test_for_each_type_counterexample_10() {
-        super::test_for_each_type!(test, [ OKType, NGType, OKType, ]);
-    }
-    #[test]
-    #[should_panic]
-    fn test_for_each_type_counterexample_11() {
-        super::test_for_each_type!(test, [ OKType, OKType, NGType ]);
-    }
-    #[test]
-    #[should_panic]
-    fn test_for_each_type_counterexample_12() {
-        super::test_for_each_type!(test, [ OKType, OKType, NGType, ]);
+        struct TestTargetType<const IS_OK: bool>;
+        impl<const IS_OK: bool> TestTargetType<IS_OK> {
+            const IS_OK: bool = IS_OK;
+        }
+        type OKType = TestTargetType<true>;
+        type NGType = TestTargetType<false>;
+        macro_rules! test {
+            ($ty: ty) => {
+                // Passing `NGType` will cause an assertion failure.
+                assert!(<$ty>::IS_OK);
+            };
+        }
+
+        #[test]
+        fn test_valid() {
+            test_for_each_type!(test, []);
+            test_for_each_type!(test, [ OKType ]);
+            test_for_each_type!(test, [ OKType, ]);
+            test_for_each_type!(test, [ OKType, OKType ]);
+            test_for_each_type!(test, [ OKType, OKType, ]);
+            test_for_each_type!(test, [ OKType, OKType, OKType ]);
+            test_for_each_type!(test, [ OKType, OKType, OKType, ]);
+        }
+        #[test]
+        #[should_panic]
+        fn test_counterexample_01() {
+            test_for_each_type!(test, [ NGType ]);
+        }
+        #[test]
+        #[should_panic]
+        fn test_counterexample_02() {
+            test_for_each_type!(test, [ NGType, ]);
+        }
+        #[test]
+        #[should_panic]
+        fn test_counterexample_03() {
+            test_for_each_type!(test, [ NGType, OKType ]);
+        }
+        #[test]
+        #[should_panic]
+        fn test_counterexample_04() {
+            test_for_each_type!(test, [ NGType, OKType, ]);
+        }
+        #[test]
+        #[should_panic]
+        fn test_counterexample_05() {
+            test_for_each_type!(test, [ OKType, NGType ]);
+        }
+        #[test]
+        #[should_panic]
+        fn test_counterexample_06() {
+            test_for_each_type!(test, [ OKType, NGType, ]);
+        }
+        #[test]
+        #[should_panic]
+        fn test_counterexample_07() {
+            test_for_each_type!(test, [ NGType, OKType, OKType ]);
+        }
+        #[test]
+        #[should_panic]
+        fn test_counterexample_08() {
+            test_for_each_type!(test, [ NGType, OKType, OKType, ]);
+        }
+        #[test]
+        #[should_panic]
+        fn test_counterexample_09() {
+            test_for_each_type!(test, [ OKType, NGType, OKType ]);
+        }
+        #[test]
+        #[should_panic]
+        fn test_counterexample_10() {
+            test_for_each_type!(test, [ OKType, NGType, OKType, ]);
+        }
+        #[test]
+        #[should_panic]
+        fn test_counterexample_11() {
+            test_for_each_type!(test, [ OKType, OKType, NGType ]);
+        }
+        #[test]
+        #[should_panic]
+        fn test_counterexample_12() {
+            test_for_each_type!(test, [ OKType, OKType, NGType, ]);
+        }
     }
 
 
     #[test]
     fn test_assert_fits_in() {
         // u8: 0..=255
-        super::assert_fits_in!(  0i32, u8);
-        super::assert_fits_in!(255u16, u8);
-        super::assert_fits_in!(255i16, u8);
-        super::assert_fits_in!(255u32, u8);
-        super::assert_fits_in!(255i32, u8);
-        super::assert_fits_in!(255u64, u8);
-        super::assert_fits_in!(255i64, u8);
+        assert_fits_in!(  0i32, u8);
+        assert_fits_in!(255u16, u8);
+        assert_fits_in!(255i16, u8);
+        assert_fits_in!(255u32, u8);
+        assert_fits_in!(255i32, u8);
+        assert_fits_in!(255u64, u8);
+        assert_fits_in!(255i64, u8);
         // i8: (-128)..=127
-        super::assert_fits_in!( 127i32, i8);
-        super::assert_fits_in!(   0i32, i8);
-        super::assert_fits_in!(-128i32, i8);
+        assert_fits_in!( 127i32, i8);
+        assert_fits_in!(   0i32, i8);
+        assert_fits_in!(-128i32, i8);
     }
 
     #[test]
     #[should_panic]
     fn test_assert_fits_in_counterexample_1() {
-        super::assert_fits_in!(256i32, u8);
+        assert_fits_in!(256i32, u8);
     }
     #[test]
     #[should_panic]
     fn test_assert_fits_in_counterexample_2() {
-        super::assert_fits_in!(-1i32, u8);
+        assert_fits_in!(-1i32, u8);
     }
     #[test]
     #[should_panic]
     fn test_assert_fits_in_counterexample_3() {
-        super::assert_fits_in!(-129i32, i8);
+        assert_fits_in!(-129i32, i8);
     }
     #[test]
     #[should_panic]
     fn test_assert_fits_in_counterexample_4() {
-        super::assert_fits_in!(128i32, i8);
+        assert_fits_in!(128i32, i8);
     }
 }
 // grcov-excl-br-end
