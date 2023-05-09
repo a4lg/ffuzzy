aed226ce199d8307a7ae25af3f5202be546517d7
Tsukasa OI <floss_ssdeep@irq.a4lg.com>
2023-05-09 03:29:12 +0000

    tests: Refine on `crate::generate_easy`
    This commit adds a test case where the input buffer size is too large.

--- ffuzzy/src/generate_easy.rs
+++ ffuzzy/src/generate_easy.rs
@@ -32,17 +32,31 @@ pub fn hash_buf(buffer: &[u8]) -> Result<RawFuzzyHash, GeneratorError> {
 
 
 // grcov-excl-br-start
-#[cfg(feature = "alloc")]
 #[cfg(test)]
 mod tests {
-    use super::*;
-
+    #[cfg(feature = "alloc")]
     #[test]
     fn test_hash_buf_usage() {
+        use crate::generate_easy::hash_buf;
         assert_eq!(
             hash_buf(b"Hello, World!\n").unwrap().to_string(),
             "3:aaX8v:aV"
         );
+    }
+
+    // TODO: Once existence of an 128-bit machine is realistic, change #[cfg] below.
+    #[cfg(target_pointer_width = "64")]
+    #[test]
+    fn test_hash_buf_too_large() {
+        use crate::generate::{Generator, GeneratorError};
+        use crate::generate_easy::hash_buf;
+        unsafe {
+            // Supply (most likely invalid) too large buffer and expect an error happens
+            // before an actual access happens (GeneratorError::FixedSizeTooLarge).
+            let too_large_size = (Generator::MAX_INPUT_SIZE + 1) as usize;
+            let buf = core::slice::from_raw_parts(core::ptr::NonNull::dangling().as_ptr(), too_large_size);
+            assert_eq!(hash_buf(buf), Err(GeneratorError::FixedSizeTooLarge));
+        }
     }
 }
 // grcov-excl-br-stop
