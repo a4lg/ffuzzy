13008fa0a00e3e5e28d66145b84c9bf2fbd7cd28
Tsukasa OI <floss_ssdeep@irq.a4lg.com>
2023-05-09 03:29:17 +0000

    tests: Refine on `crate::compare_easy`
    Along with `crate::hash::parser_state` refinements, the tests are organized
    and the robustness is improved.  It also reuses the test data
    from `crate::hash::parser_state::tests`.
    
    This commit also splits tests in `compare_easy.rs`
    to `compare_easy/tests.rs`.

--- ffuzzy/src/compare_easy.rs
+++ ffuzzy/src/compare_easy.rs
@@ -9,6 +9,10 @@ use crate::hash::parser_state::{
 };
 
 
+#[cfg(test)]
+mod tests;
+
+
 /// The operand (side) which caused a parse error.
 #[repr(u8)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -80,122 +84,3 @@ pub fn compare(lhs: &str, rhs: &str) -> Result<u32, ParseErrorEither> {
     };
     Ok(lhs.compare(rhs.as_ref()))
 }
-
-
-
-
-
-// grcov-excl-br-start
-#[cfg(test)]
-mod tests {
-    use super::*;
-    #[cfg(feature = "alloc")]
-    use alloc::format;
-    use crate::hash::parser_state::{ParseError, ParseErrorKind, ParseErrorOrigin};
-    use crate::test_utils::test_auto_clone;
-
-    #[test]
-    fn test_parse_error_side_basic() {
-        test_auto_clone::<ParseErrorSide>(&ParseErrorSide::Left);
-        #[cfg(feature = "alloc")]
-        {
-            crate::test_utils::test_auto_debug_for_enum!(
-                ParseErrorSide,
-                [
-                    Left,
-                    Right,
-                ]
-            );
-        }
-    }
-
-    #[test]
-    fn test_parse_error_either_basic() {
-        let err = ParseErrorEither(
-            ParseErrorSide::Left,
-            ParseError(ParseErrorKind::UnexpectedEndOfString, ParseErrorOrigin::BlockHash1, 2)
-        );
-        test_auto_clone::<ParseErrorEither>(&err);
-        assert_eq!(err.side(), ParseErrorSide::Left);
-        assert_eq!(err.kind(), ParseErrorKind::UnexpectedEndOfString);
-        assert_eq!(err.origin(), ParseErrorOrigin::BlockHash1);
-        assert_eq!(err.offset(), 2);
-        #[cfg(feature = "alloc")]
-        {
-            assert_eq!(
-                format!("{:?}", err),
-                "ParseErrorEither(Left, ParseError(UnexpectedEndOfString, BlockHash1, 2))"
-            );
-        }
-    }
-
-    #[test]
-    fn test_compare_ok() {
-        assert_eq!(
-            compare(
-                "6:3ll7QzDkmJmMHkQoO/llSZEnEuLszmbMAWn:VqDk5QtLbW",
-                "6:3ll7QzDkmQjmMoDHglHOxPWT0lT0lT0lB:VqDk+n"
-            ).unwrap(),
-            46
-        );
-    }
-
-    #[test]
-    fn test_compare_err() {
-        // Valid:   "3::"
-        // Invalid: "3,::"
-        // Invalid: "4::"
-        assert_eq!(compare("3,::", "3::"), Err(ParseErrorEither(
-            ParseErrorSide::Left,
-            ParseError(ParseErrorKind::UnexpectedCharacter, ParseErrorOrigin::BlockSize, 1))
-        ));
-        assert_eq!(compare("3::", "4::"), Err(ParseErrorEither(
-            ParseErrorSide::Right,
-            ParseError(ParseErrorKind::BlockSizeIsInvalid, ParseErrorOrigin::BlockSize, 0))
-        ));
-        // An error with ParseErrorSide::Left on the current implementation
-        // but this property is not guaranteed
-        // (just make sure that this is an error).
-        assert!(compare("3,::", "4::").is_err());
-    }
-
-    #[test]
-    fn test_compare_error_side() {
-        let hstr_valid = "3::";
-        let hstr_err_empty = "";
-        let hstr_err_eos_bh1 = "3:";
-        let hstr_err_bs_invalid = "4::";
-        let err_empty = ParseError(ParseErrorKind::UnexpectedEndOfString, ParseErrorOrigin::BlockSize, 0);
-        let err_eos_bh1 = ParseError(ParseErrorKind::UnexpectedEndOfString, ParseErrorOrigin::BlockHash1, 2);
-        let err_bs_invalid = ParseError(ParseErrorKind::BlockSizeIsInvalid, ParseErrorOrigin::BlockSize, 0);
-        // Left side has an error.
-        assert_eq!(compare(hstr_err_empty,      hstr_valid), Err(ParseErrorEither(ParseErrorSide::Left, err_empty)));
-        assert_eq!(compare(hstr_err_eos_bh1,    hstr_valid), Err(ParseErrorEither(ParseErrorSide::Left, err_eos_bh1)));
-        assert_eq!(compare(hstr_err_bs_invalid, hstr_valid), Err(ParseErrorEither(ParseErrorSide::Left, err_bs_invalid)));
-        // Right side has an error.
-        assert_eq!(compare(hstr_valid, hstr_err_empty),      Err(ParseErrorEither(ParseErrorSide::Right, err_empty)));
-        assert_eq!(compare(hstr_valid, hstr_err_eos_bh1),    Err(ParseErrorEither(ParseErrorSide::Right, err_eos_bh1)));
-        assert_eq!(compare(hstr_valid, hstr_err_bs_invalid), Err(ParseErrorEither(ParseErrorSide::Right, err_bs_invalid)));
-    }
-
-    #[cfg(feature = "alloc")]
-    #[test]
-    fn test_compare_error_str() {
-        let err_empty = ParseError(ParseErrorKind::UnexpectedEndOfString, ParseErrorOrigin::BlockSize, 0);
-        let err_eos_bh1 = ParseError(ParseErrorKind::UnexpectedEndOfString, ParseErrorOrigin::BlockHash1, 2);
-        let err_bs_invalid = ParseError(ParseErrorKind::BlockSizeIsInvalid, ParseErrorOrigin::BlockSize, 0);
-        assert_eq!(format!("{}", ParseErrorEither(ParseErrorSide::Left, err_empty)),
-            "error occurred while parsing fuzzy hash 1 (block size, at byte offset 0): end-of-string is not expected");
-        assert_eq!(format!("{}", ParseErrorEither(ParseErrorSide::Right, err_empty)),
-            "error occurred while parsing fuzzy hash 2 (block size, at byte offset 0): end-of-string is not expected");
-        assert_eq!(format!("{}", ParseErrorEither(ParseErrorSide::Left, err_eos_bh1)),
-            "error occurred while parsing fuzzy hash 1 (block hash 1, at byte offset 2): end-of-string is not expected");
-        assert_eq!(format!("{}", ParseErrorEither(ParseErrorSide::Right, err_eos_bh1)),
-            "error occurred while parsing fuzzy hash 2 (block hash 1, at byte offset 2): end-of-string is not expected");
-        assert_eq!(format!("{}", ParseErrorEither(ParseErrorSide::Left, err_bs_invalid)),
-            "error occurred while parsing fuzzy hash 1 (block size, at byte offset 0): block size is not valid");
-        assert_eq!(format!("{}", ParseErrorEither(ParseErrorSide::Right, err_bs_invalid)),
-            "error occurred while parsing fuzzy hash 2 (block size, at byte offset 0): block size is not valid");
-    }
-}
-// grcov-excl-br-end
---
+++ ffuzzy/src/compare_easy/tests.rs
@@ -0,0 +1,134 @@
+// SPDX-License-Identifier: MIT
+// SPDX-FileCopyrightText: Copyright (C) 2023 Tsukasa OI <floss_ssdeep@irq.a4lg.com>.
+// grcov-excl-br-start
+
+#![cfg(test)]
+
+#[cfg(feature = "alloc")]
+use alloc::format;
+use crate::compare_easy::{compare, ParseErrorSide, ParseErrorEither};
+use crate::hash::parser_state::{ParseError, ParseErrorKind, ParseErrorOrigin, ParseErrorInfo};
+use crate::test_utils::test_auto_clone;
+
+
+#[test]
+fn test_parse_error_side() {
+    // Test Clone
+    test_auto_clone::<ParseErrorSide>(&ParseErrorSide::Left);
+    #[cfg(feature = "alloc")]
+    {
+        // Test Debug
+        crate::test_utils::test_auto_debug_for_enum!(
+            ParseErrorSide,
+            [
+                Left,
+                Right,
+            ]
+        );
+    }
+}
+
+#[test]
+fn test_parse_error_either_basic() {
+    // Internal values
+    const SIDE: ParseErrorSide     = ParseErrorSide::Left;
+    const KIND: ParseErrorKind     = ParseErrorKind::UnexpectedEndOfString;
+    const ORIGIN: ParseErrorOrigin = ParseErrorOrigin::BlockHash1;
+    const OFFSET: usize = 2;
+    // Construct an error object.
+    let err = ParseErrorEither(SIDE, ParseError(KIND, ORIGIN, OFFSET));
+    // Test Clone
+    test_auto_clone::<ParseErrorEither>(&err);
+    // Check internal values.
+    assert_eq!(err.side(), SIDE);
+    assert_eq!(err.kind(), KIND);
+    assert_eq!(err.origin(), ORIGIN);
+    assert_eq!(err.offset(), OFFSET);
+    #[cfg(feature = "alloc")]
+    {
+        // Test Debug
+        assert_eq!(
+            format!("{:?}", err),
+            "ParseErrorEither(\
+                Left, \
+                ParseError(\
+                    UnexpectedEndOfString, \
+                    BlockHash1, \
+                    2\
+                )\
+            )"
+        );
+    }
+}
+
+#[cfg(feature = "alloc")]
+#[test]
+fn test_parse_error_either_display_and_debug_with_side() {
+    for (err, err_str_display, err_str_debug) in crate::hash::parser_state::tests::PARSE_ERROR_CASES {
+        // Test Display
+        assert_eq!(
+            format!("{}", ParseErrorEither(ParseErrorSide::Left, err)),
+            format!("error occurred while parsing fuzzy hash 1 {}", err_str_display)
+        );
+        assert_eq!(
+            format!("{}", ParseErrorEither(ParseErrorSide::Right, err)),
+            format!("error occurred while parsing fuzzy hash 2 {}", err_str_display)
+        );
+        // Test Debug
+        assert_eq!(
+            format!("{:?}", ParseErrorEither(ParseErrorSide::Left, err)),
+            format!("ParseErrorEither(Left, {})", err_str_debug)
+        );
+        assert_eq!(
+            format!("{:?}", ParseErrorEither(ParseErrorSide::Right, err)),
+            format!("ParseErrorEither(Right, {})", err_str_debug)
+        );
+    }
+}
+
+#[test]
+fn test_compare_ok() {
+    assert_eq!(
+        compare(
+            "6:3ll7QzDkmJmMHkQoO/llSZEnEuLszmbMAWn:VqDk5QtLbW",
+            "6:3ll7QzDkmQjmMoDHglHOxPWT0lT0lT0lB:VqDk+n"
+        ).unwrap(),
+        46
+    );
+}
+
+#[test]
+fn test_compare_error() {
+    const STR_VALID: &str = "3::";
+    const ERROR_CASES: [(&str, ParseError); 3] = [
+        ("",    ParseError(ParseErrorKind::UnexpectedEndOfString, ParseErrorOrigin::BlockSize,  0)),
+        ("3:",  ParseError(ParseErrorKind::UnexpectedEndOfString, ParseErrorOrigin::BlockHash1, 2)),
+        ("4::", ParseError(ParseErrorKind::BlockSizeIsInvalid,    ParseErrorOrigin::BlockSize,  0)),
+    ];
+    for (hash_str_invalid, err) in ERROR_CASES {
+        // Left side has an error.
+        assert_eq!(compare(hash_str_invalid, STR_VALID), Err(ParseErrorEither(ParseErrorSide::Left, err)),
+            "test case failed on the invalid hash string {:?} on the left", hash_str_invalid);
+        // Right side has an error.
+        assert_eq!(compare(STR_VALID, hash_str_invalid), Err(ParseErrorEither(ParseErrorSide::Right, err)),
+            "test case failed on the invalid hash string {:?} on the right", hash_str_invalid);
+    }
+    /*
+        If both sides are invalid, an error with ParseErrorSide::Left is
+        generated on the current implementation but this property is not
+        guaranteed by any version.
+        It just make sures that this is an actual error generated in an
+        consistent manner (X side + parse error on the X side).
+    */
+    for (hash_str_invalid_l, err_l) in ERROR_CASES {
+        for (hash_str_invalid_r, err_r) in ERROR_CASES {
+            let err = compare(hash_str_invalid_l, hash_str_invalid_r);
+            // grcov-excl-start: Not very relevant to the true coverage.
+            assert!(
+                err == Err(ParseErrorEither(ParseErrorSide::Left,  err_l)) ||
+                err == Err(ParseErrorEither(ParseErrorSide::Right, err_r))
+            );
+            // grcov-excl-end
+        }
+    }
+}
