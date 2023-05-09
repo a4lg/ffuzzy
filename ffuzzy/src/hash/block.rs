bf7a0ab769e2d4194e0ddb5f386d3ff24f136d0d
Tsukasa OI <floss_ssdeep@irq.a4lg.com>
2023-05-09 03:29:12 +0000

    tests: Refine on `crate::hash::block`
    New tests will test all methods and (optionally) all `u32` values as block
    sizes.

--- ffuzzy/src/hash/block.rs
+++ ffuzzy/src/hash/block.rs
@@ -467,6 +467,9 @@ mod const_asserts {
     // fuzzy.c: (implementation of memcpy_eliminate_sequences)
     const_assert_eq!(BlockHash::MAX_SEQUENCE_SIZE, 3);
 
+    // NUM_VALID + 1 must be a valid u8 value.
+    const_assert_ne!(BlockSize::NUM_VALID as u8, u8::MAX);
+
     // MAX_SEQUENCE_SIZE: fits in u32 and safe to add 1 (in either u32 or usize)
     const_assert!(BlockHash::MAX_SEQUENCE_SIZE < 0xffff_ffff);
     const_assert_ne!(BlockHash::MAX_SEQUENCE_SIZE, usize::MAX);
@@ -485,52 +488,179 @@ mod tests {
     use alloc::string::ToString;
 
     #[test]
-    fn test_block_size_log_valid() {
-        for i in 0..BlockSize::NUM_VALID as u8 {
-            // log(exp(i)) == i.
-            assert_eq!(
-                i,
-                BlockSize::log_from_valid(
-                    BlockSize::from_log(i).unwrap()
+    fn test_prerequisites() {
+        // NUM_VALID and NUM_VALID + 1 must be valid u8 values.
+        crate::test_utils::assert_fits_in!(BlockSize::NUM_VALID, u8);
+        crate::test_utils::assert_fits_in!(BlockSize::NUM_VALID + 1, u8);
+    }
+
+    #[test]
+    fn test_block_size_validness_border() {
+        const WIDTH: u32 = 10;
+        for log_block_size in 0..BlockSize::NUM_VALID as u8 {
+            let block_size = BlockSize::from_log_internal(log_block_size);
+            let block_size_prev_plus_1 =
+                if log_block_size > 0 {
+                    BlockSize::from_log_internal(log_block_size - 1) + 1
+                }
+                else {
+                    0
+                };
+            let block_size_next_minus_1 =
+                if BlockSize::is_log_valid(log_block_size + 1) {
+                    BlockSize::from_log_internal(log_block_size + 1) - 1
+                }
+                else {
+                    u32::MAX
+                };
+            for bs in
+                u32::max(
+                    block_size.saturating_sub(WIDTH),
+                    block_size_prev_plus_1
+                )..block_size
+            {
+                assert!(!BlockSize::is_valid(bs), "failed on bs={:?}", bs);
+            }
+            assert!(BlockSize::is_valid(block_size), "failed on block_size={:?}", block_size);
+            if block_size == u32::MAX { continue; }
+            for bs in
+                (block_size + 1)..=u32::min(
+                    block_size.saturating_add(WIDTH),
+                    block_size_next_minus_1
                 )
-            );
-            #[cfg(feature = "unsafe")]
-            unsafe {
-                assert_eq!(
-                    i,
-                    BlockSize::log_from_valid_unchecked(
-                        BlockSize::from_log(i).unwrap()
-                    )
-                );
+            {
+                assert!(!BlockSize::is_valid(bs), "failed on bs={:?}", bs);
+            }
+        }
+    }
+
+    #[cfg(feature = "tests-slow")]
+    #[test]
+    fn test_block_size_validness_all() {
+        assert!(!BlockSize::is_valid(0));
+        let mut next_log_block_size = 0;
+        let mut next_block_size_minus_1 = BlockSize::from_log_internal(next_log_block_size) - 1;
+        let mut test_next = false;
+        for block_size in u32::MIN..=u32::MAX {
+            if test_next {
+                assert!(BlockSize::is_valid(block_size),
+                    "failed on block_size={:?}", block_size);
+                test_next = false;
+            }
+            else {
+                assert!(!BlockSize::is_valid(block_size),
+                    "failed on block_size={:?}", block_size);
+            }
+            if block_size == next_block_size_minus_1 {
+                test_next = true;
+                next_log_block_size += 1;
+                next_block_size_minus_1 =
+                    if BlockSize::is_log_valid(next_log_block_size) {
+                        BlockSize::from_log_internal(next_log_block_size) - 1
+                    }
+                    else {
+                        u32::MAX
+                    };
             }
         }
     }
 
     #[test]
-    fn test_block_size_log_invalid() {
-        for i in BlockSize::NUM_VALID as u8..=u8::MAX {
-            assert_eq!(BlockSize::from_log(i), None);
+    fn test_block_size_log_valid() {
+        for log_block_size in 0..BlockSize::NUM_VALID as u8 {
+            assert!(BlockSize::is_log_valid(log_block_size),
+                "failed on log_block_size={:?}", log_block_size);
+            // exp(i)
+            let block_size = {
+                let block_size_1 = BlockSize::from_log(log_block_size).unwrap();
+                let block_size_2 = BlockSize::from_log_internal(log_block_size);
+                assert_eq!(block_size_1, block_size_2,
+                    "failed on log_block_size={:?}", log_block_size);
+                #[cfg(feature = "unsafe")]
+                unsafe {
+                    let block_size_3 = BlockSize::from_log_unchecked(log_block_size);
+                    assert_eq!(block_size_1, block_size_3,
+                        "failed on log_block_size={:?}", log_block_size);
+                }
+                block_size_1
+            };
+            assert!(BlockSize::is_valid(block_size),
+                "failed on log_block_size={:?}", log_block_size);
+            // log(exp(i)) == i.
+            assert_eq!(
+                log_block_size,
+                BlockSize::log_from_valid(block_size),
+                "failed on log_block_size={:?}", log_block_size
+            );
+            assert_eq!(
+                log_block_size,
+                BlockSize::log_from_valid_internal(block_size),
+                "failed on log_block_size={:?}", log_block_size
+            );
+            #[cfg(feature = "unsafe")]
+            unsafe {
+                assert_eq!(
+                    log_block_size,
+                    BlockSize::log_from_valid_unchecked(block_size),
+                    "failed on log_block_size={:?}", log_block_size
+                );
+            }
+            // Relations with BlockSize::MIN
+            assert_eq!(block_size % BlockSize::MIN, 0,
+                "failed on log_block_size={:?}", log_block_size);
+            assert!((block_size / BlockSize::MIN).is_power_of_two(),
+                "failed on log_block_size={:?}", log_block_size);
+            assert_eq!(
+                u8::try_from(crate::utils::u64_ilog2((block_size / BlockSize::MIN) as u64)).unwrap(),
+                log_block_size,
+                "failed on log_block_size={:?}", log_block_size);
         }
     }
 
-    #[cfg(feature = "alloc")]
+    #[test]
+    fn test_block_size_log_invalid() {
+        for log_block_size in BlockSize::NUM_VALID as u8..=u8::MAX {
+            assert!(!BlockSize::is_log_valid(log_block_size),
+                "failed on log_block_size={:?}", log_block_size);
+            assert_eq!(BlockSize::from_log(log_block_size), None,
+                "failed on log_block_size={:?}", log_block_size);
+        }
+    }
+
     #[test]
     fn test_block_size_strings() {
-        for i in 0..BlockSize::NUM_VALID as u8 {
-            // BLOCK_SIZES_STR must match to converted strings from the block size.
-            let block_size = BlockSize::from_log(i).unwrap();
-            assert_eq!(BlockSize::BLOCK_SIZES_STR[i as usize], block_size.to_string());
-            assert!(BlockSize::BLOCK_SIZES_STR[i as usize].len() <= BlockSize::MAX_BLOCK_SIZE_LEN_IN_CHARS);
+        // Prerequisites
+        assert_eq!(BlockSize::NUM_VALID, BlockSize::BLOCK_SIZES_STR.len());
+        // Test all valid *base-2 logarithm* values.
+        for log_block_size in 0..BlockSize::NUM_VALID {
+            // BLOCK_SIZES_STR[i] must have direct correspondence with valid block size.
+            let block_size = BlockSize::from_log(log_block_size as u8).unwrap();
+            let block_size_from_str: u32 = str::parse(BlockSize::BLOCK_SIZES_STR[log_block_size]).unwrap();
+            assert_eq!(block_size, block_size_from_str,
+                "failed on log_block_size={:?}", log_block_size);
+            // The length must be bounded by MAX_BLOCK_SIZE_LEN_IN_CHARS.
+            assert!(BlockSize::BLOCK_SIZES_STR[log_block_size].len() <= BlockSize::MAX_BLOCK_SIZE_LEN_IN_CHARS,
+                "failed on log_block_size={:?}", log_block_size);
+            #[cfg(feature = "alloc")]
+            {
+                assert_eq!(BlockSize::BLOCK_SIZES_STR[log_block_size], block_size.to_string(),
+                    "failed on log_block_size={:?}", log_block_size);
+            }
         }
     }
 
 
-    #[cfg(feature = "alloc")]
     #[test]
-    fn test_block_size_relation_debug() {
-        crate::test_utils::test_auto_debug_for_enum!(
-            BlockSizeRelation, [Far, NearEq, NearGt, NearLt]
-        );
+    fn test_block_size_relation_basic() {
+        // Test Clone
+        crate::test_utils::test_auto_clone::<BlockSizeRelation>(&BlockSizeRelation::Far);
+        #[cfg(feature = "alloc")]
+        {
+            // Test Debug
+            crate::test_utils::test_auto_debug_for_enum!(
+                BlockSizeRelation, [Far, NearEq, NearGt, NearLt]
+            );
+        }
     }
 
     #[test]
@@ -540,52 +670,53 @@ mod tests {
                 // Use cmp.
                 let ord = BlockSize::cmp(bs1, bs2);
                 match ord {
-                    Ordering::Equal   => assert!(bs1 == bs2),
-                    Ordering::Less    => assert!(bs1 < bs2),
-                    Ordering::Greater => assert!(bs1 > bs2),
+                    Ordering::Equal   => assert!(bs1 == bs2, "failed on bs1={:?}, bs2={:?}", bs1, bs2),
+                    Ordering::Less    => assert!(bs1 < bs2,  "failed on bs1={:?}, bs2={:?}", bs1, bs2),
+                    Ordering::Greater => assert!(bs1 > bs2,  "failed on bs1={:?}, bs2={:?}", bs1, bs2),
                 }
                 // Use compare_sizes.
                 let rel = BlockSize::compare_sizes(bs1, bs2);
                 // Test consistency between logical expressions and the BlockSizeRelation value.
                 // TODO: Replace plain subtraction with abs_diff when MSRV 1.60 is acceptable.
-                assert_eq!(bs1 == bs2, rel == BlockSizeRelation::NearEq);
-                assert_eq!(bs1 == bs2 + 1, rel == BlockSizeRelation::NearGt);
-                assert_eq!(bs1 + 1 == bs2, rel == BlockSizeRelation::NearLt);
-                assert_eq!(((bs1 as i32) - (bs2 as i32)).abs() > 1, rel == BlockSizeRelation::Far);
+                assert_eq!(bs1 == bs2, rel == BlockSizeRelation::NearEq,     "failed on bs1={:?}, bs2={:?}", bs1, bs2);
+                assert_eq!(bs1 == bs2 + 1, rel == BlockSizeRelation::NearGt, "failed on bs1={:?}, bs2={:?}", bs1, bs2);
+                assert_eq!(bs1 + 1 == bs2, rel == BlockSizeRelation::NearLt, "failed on bs1={:?}, bs2={:?}", bs1, bs2);
+                assert_eq!(((bs1 as i32) - (bs2 as i32)).abs() > 1, rel == BlockSizeRelation::Far,
+                    "failed on bs1={:?}, bs2={:?}", bs1, bs2);
                 // Test consistency between the result of other functions and the BlockSizeRelation value.
                 #[allow(clippy::bool_assert_comparison)]
-                match rel.clone() {
+                match rel {
                     BlockSizeRelation::Far => {
-                        assert_eq!(rel.is_near(), false);
-                        assert_eq!(BlockSize::is_near(bs1, bs2), false);
-                        assert_eq!(BlockSize::is_near_lt(bs1, bs2), false);
-                        assert_eq!(BlockSize::is_near_eq(bs1, bs2), false);
-                        assert_eq!(BlockSize::is_near_gt(bs1, bs2), false);
-                        assert_ne!(ord, Ordering::Equal);
+                        assert_eq!(rel.is_near(), false,                   "failed on bs1={:?}, bs2={:?}", bs1, bs2);
+                        assert_eq!(BlockSize::is_near(bs1, bs2), false,    "failed on bs1={:?}, bs2={:?}", bs1, bs2);
+                        assert_eq!(BlockSize::is_near_lt(bs1, bs2), false, "failed on bs1={:?}, bs2={:?}", bs1, bs2);
+                        assert_eq!(BlockSize::is_near_eq(bs1, bs2), false, "failed on bs1={:?}, bs2={:?}", bs1, bs2);
+                        assert_eq!(BlockSize::is_near_gt(bs1, bs2), false, "failed on bs1={:?}, bs2={:?}", bs1, bs2);
+                        assert_ne!(ord, Ordering::Equal,                   "failed on bs1={:?}, bs2={:?}", bs1, bs2);
                     }
                     BlockSizeRelation::NearLt => {
-                        assert_eq!(rel.is_near(), true);
-                        assert_eq!(BlockSize::is_near(bs1, bs2), true);
-                        assert_eq!(BlockSize::is_near_lt(bs1, bs2), true);
-                        assert_eq!(BlockSize::is_near_eq(bs1, bs2), false);
-                        assert_eq!(BlockSize::is_near_gt(bs1, bs2), false);
-                        assert_eq!(ord, Ordering::Less);
+                        assert_eq!(rel.is_near(), true,                    "failed on bs1={:?}, bs2={:?}", bs1, bs2);
+                        assert_eq!(BlockSize::is_near(bs1, bs2), true,     "failed on bs1={:?}, bs2={:?}", bs1, bs2);
+                        assert_eq!(BlockSize::is_near_lt(bs1, bs2), true,  "failed on bs1={:?}, bs2={:?}", bs1, bs2);
+                        assert_eq!(BlockSize::is_near_eq(bs1, bs2), false, "failed on bs1={:?}, bs2={:?}", bs1, bs2);
+                        assert_eq!(BlockSize::is_near_gt(bs1, bs2), false, "failed on bs1={:?}, bs2={:?}", bs1, bs2);
+                        assert_eq!(ord, Ordering::Less,                    "failed on bs1={:?}, bs2={:?}", bs1, bs2);
                     }
                     BlockSizeRelation::NearEq => {
-                        assert_eq!(rel.is_near(), true);
-                        assert_eq!(BlockSize::is_near(bs1, bs2), true);
-                        assert_eq!(BlockSize::is_near_lt(bs1, bs2), false);
-                        assert_eq!(BlockSize::is_near_eq(bs1, bs2), true);
-                        assert_eq!(BlockSize::is_near_gt(bs1, bs2), false);
-                        assert_eq!(ord, Ordering::Equal);
+                        assert_eq!(rel.is_near(), true,                    "failed on bs1={:?}, bs2={:?}", bs1, bs2);
+                        assert_eq!(BlockSize::is_near(bs1, bs2), true,     "failed on bs1={:?}, bs2={:?}", bs1, bs2);
+                        assert_eq!(BlockSize::is_near_lt(bs1, bs2), false, "failed on bs1={:?}, bs2={:?}", bs1, bs2);
+                        assert_eq!(BlockSize::is_near_eq(bs1, bs2), true,  "failed on bs1={:?}, bs2={:?}", bs1, bs2);
+                        assert_eq!(BlockSize::is_near_gt(bs1, bs2), false, "failed on bs1={:?}, bs2={:?}", bs1, bs2);
+                        assert_eq!(ord, Ordering::Equal,                   "failed on bs1={:?}, bs2={:?}", bs1, bs2);
                     }
                     BlockSizeRelation::NearGt => {
-                        assert_eq!(rel.is_near(), true);
-                        assert_eq!(BlockSize::is_near(bs1, bs2), true);
-                        assert_eq!(BlockSize::is_near_lt(bs1, bs2), false);
-                        assert_eq!(BlockSize::is_near_eq(bs1, bs2), false);
-                        assert_eq!(BlockSize::is_near_gt(bs1, bs2), true);
-                        assert_eq!(ord, Ordering::Greater);
+                        assert_eq!(rel.is_near(), true,                    "failed on bs1={:?}, bs2={:?}", bs1, bs2);
+                        assert_eq!(BlockSize::is_near(bs1, bs2), true,     "failed on bs1={:?}, bs2={:?}", bs1, bs2);
+                        assert_eq!(BlockSize::is_near_lt(bs1, bs2), false, "failed on bs1={:?}, bs2={:?}", bs1, bs2);
+                        assert_eq!(BlockSize::is_near_eq(bs1, bs2), false, "failed on bs1={:?}, bs2={:?}", bs1, bs2);
+                        assert_eq!(BlockSize::is_near_gt(bs1, bs2), true,  "failed on bs1={:?}, bs2={:?}", bs1, bs2);
+                        assert_eq!(ord, Ordering::Greater,                 "failed on bs1={:?}, bs2={:?}", bs1, bs2);
                     }
                 }
             }
