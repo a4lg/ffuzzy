8d2d65e2c70e1072b603e0dac9fe7edcb7830d41
Tsukasa OI <floss_ssdeep@irq.a4lg.com>
2023-06-29 01:48:55 +0000

    Refine de Bruijn sequence finder / table generator
    *   (A bit of) Generalization
    *   Do not skip even numbers
    *   Much lower bound to search
        (the previous "lower bound" ignored `table[0] == 0` case)

--- dev/generator/hash-blocksize-debruijn/src/main.rs
+++ dev/generator/hash-blocksize-debruijn/src/main.rs
@@ -4,24 +4,37 @@
 use std::process;
 
 fn main() {
-    for debruijn_mul in ((((1u32 << 27) / 3) | 1)..=u32::MAX).step_by(2) {
+    const MULTIPLIER: u32 = 3;
+    const BITS_TO_COVER: u32 = MULTIPLIER.leading_zeros() + 1;
+    const SHIFT_WIDTH: u32 = u32::BITS.leading_zeros() + 1;
+
+    // Parameter constraints for ssdeep
+    assert!(MULTIPLIER == 3);
+    assert!(BITS_TO_COVER == 31);
+    assert!(SHIFT_WIDTH == 27);
+
+    // Search a variant of de Bruijn sequence
+    fn debruijn_index(index: u32, debruijn_mul: u32) -> u32 {
+        (MULTIPLIER << index).wrapping_mul(debruijn_mul) >> SHIFT_WIDTH
+    }
+    for debruijn_mul in ((1u32 << (SHIFT_WIDTH - 1)) / MULTIPLIER)..=u32::MAX {
         let mut table_fill: u32 = 0;
-        for i in 0..31 {
-            table_fill |= 1u32 << ((3u32 << i).wrapping_mul(debruijn_mul) >> 27);
+        for i in 0..BITS_TO_COVER {
+            table_fill |= 1u32 << debruijn_index(i, debruijn_mul);
         }
-        if table_fill.count_ones() == 31 {
+        if table_fill.count_ones() == BITS_TO_COVER {
             let mut table: [u8; 32] = [0xff; 32];
-            for i in 0..31 {
-                table[((3u32 << i).wrapping_mul(debruijn_mul) >> 27) as usize] = i as u8;
+            for i in 0..BITS_TO_COVER {
+                table[debruijn_index(i, debruijn_mul) as usize] = i as u8;
             }
             println!("const LOG_DEBRUIJN_CONSTANT: u32 = 0x{:08x};", debruijn_mul);
             println!("const LOG_DEBRUIJN_TABLE: [u8; 32] = [");
-            for j in 0..32 {
+            for j in 0..table.len() {
                 if j % 8 == 0 {
                     print!("   ");
                 }
                 print!(" 0x{:02x},", table[j]);
-                if j % 8 == 7 {
+                if j % 8 == 7 || j == table.len() - 1 {
                     println!("");
                 }
             }
