57cfd4c8f3c1ff0a305d7f9ef6312c846e5db9c0
Tsukasa OI <floss_ssdeep@irq.a4lg.com>
2024-01-20 05:54:15 +0000

    Remove misleading SPDX tags
    Documentation itself is **not** in the public domain.
    To avoid confusion, it removes CC0-1.0 SPDX tags from certain files.

--- ffuzzy/src/docs.rs
+++ ffuzzy/src/docs.rs
@@ -1,6 +1,3 @@
-// SPDX-License-Identifier: CC0-1.0
-// SPDX-FileCopyrightText: Authored by Tsukasa OI <floss_ssdeep@irq.a4lg.com> in 2023
-
 //! Provides additional documentation (and no code).
 
 #![cfg(doc)]
