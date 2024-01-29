a4lg/ffuzzy

commit 975dd27d29928aaf2461cbc4942bd0b86938ab08

hash: Optimize `parse_block_hash_from_bytes()`
Although no significant performance change is expected, it will help
making the code slightly smaller.

1.  (On `nightly` only) mark error branch as `unlikely`
2.  Split the "pre-return" phase into two separate phases
    (phase 2 is not mandatory if the parser is guaranteed to fail).

ffuzzy/src/hash/algorithms.rs
@@ -10,6 +10,7 @@ use crate::hash::block::{
 use crate::hash::parser_state::{
     BlockHashParseState, ParseError, ParseErrorKind, ParseErrorOrigin
 };
+use crate::intrinsics::unlikely;
 use crate::macros::{optionally_unsafe, invariant};
 
 
@@ -229,6 +230,9 @@ pub(crate) fn parse_block_size_from_bytes(bytes: &[u8], i: &mut usize)
 /// and we have found a sequence longer than
 /// [`MAX_SEQUENCE_SIZE`](block_hash::MAX_SEQUENCE_SIZE).
 ///
+/// Note that however, this function *may not* be called when we once know
+/// that the parsing the block hash results in a failure.
+///
 /// Arguments to `report_norm_seq` is as follows:
 ///
 /// 1.  The offset in the *normalized* block hash
@@ -252,8 +256,12 @@ where
     let mut prev = BASE64_INVALID;
     let mut j = *i;
     let mut len: usize = 0;
-    macro_rules! pre_ret {() => {
+    // Mandatory before returning.
+    macro_rules! pre_ret_1 {() => {
         *blockhash_len = len as u8;
+    }}
+    // Not mandatory if the parsing is guaranteed to fail.
+    macro_rules! pre_ret_2 {() => {
         if NORM && seq == block_hash::MAX_SEQUENCE_SIZE {
             let len = j - seq_start_in;
             report_norm_seq(seq_start, len);
@@ -286,24 +294,25 @@ where
                         prev = curr;
                     }
                 }
-                if len >= N {
-                    pre_ret!();
+                if unlikely(len >= N) {
+                    pre_ret_1!();
                     ret!(BlockHashParseState::OverflowError);
                 }
                 blockhash[len] = curr; // grcov-excl-br-line:ARRAY
                 len += 1;
             }
             else {
-                pre_ret!();
+                pre_ret_1!();
                 match ch {
-                    b':' => { j += 1; ret!(BlockHashParseState::MetColon); }
-                    b',' => { j += 1; ret!(BlockHashParseState::MetComma); }
+                    b':' => { j += 1; pre_ret_2!(); ret!(BlockHashParseState::MetColon); }
+                    b',' => { j += 1; pre_ret_2!(); ret!(BlockHashParseState::MetComma); }
                     _ => { ret!(BlockHashParseState::Base64Error); }
                 }
             }
             j += 1;
         }
     }
-    pre_ret!();
+    pre_ret_1!();
+    pre_ret_2!();
     ret!(BlockHashParseState::MetEndOfString);
 }

commit b2554ddd3fd633355f7d94e154ff4a3d2a966506

Fix optimization to `parse_block_hash_from_bytes()`
Commit 975dd27d2992 broke the parser for mistakenly placing
`pre_ret_2!()` after incrementing `j` (that will result in the incorrect
tail handling).

Also, it needed some test changes because the test did not handle the
case which failed to parse the block hash.

This commit fixes those.

ffuzzy/src/hash/algorithms.rs
@@ -304,8 +304,8 @@ where
             else {
                 pre_ret_1!();
                 match ch {
-                    b':' => { j += 1; pre_ret_2!(); ret!(BlockHashParseState::MetColon); }
-                    b',' => { j += 1; pre_ret_2!(); ret!(BlockHashParseState::MetComma); }
+                    b':' => { pre_ret_2!(); j += 1; ret!(BlockHashParseState::MetColon); }
+                    b',' => { pre_ret_2!(); j += 1; ret!(BlockHashParseState::MetComma); }
                     _ => { ret!(BlockHashParseState::Base64Error); }
                 }
             }

ffuzzy/src/hash/algorithms/tests.rs
@@ -362,7 +362,15 @@ fn parse_block_hash_from_bytes_states_and_normalization_reporting() {
                 reported_seqs.push((start_pos_norm, seq_len));
             }
         );
-        assert_eq!(&reported_seqs, expected_reported_seqs, "failed on bytes={:?}", bytes);
+        match state {
+            BlockHashParseState::Base64Error | BlockHashParseState::OverflowError => {
+                // On error, the last run-length may not be reported but otherwise the same.
+                assert!(
+                    &reported_seqs == expected_reported_seqs || reported_seqs == expected_reported_seqs[..expected_reported_seqs.len() - 1],
+                    "failed on bytes={:?}", bytes);
+            }
+            _ => { assert_eq!(&reported_seqs, expected_reported_seqs, "failed on bytes={:?}", bytes); }
+        }
         assert_eq!(state, expected_state, "failed on bytes={:?}", bytes);
         assert_eq!(&buffer, &expected_buffer, "failed on bytes={:?}", bytes);
         assert_eq!(input_offset, expected_input_offset, "failed on bytes={:?}", bytes);