b5786e24c03e99090332ee9f97a5e6943a3f4cb3
Tsukasa OI <floss_ssdeep@irq.a4lg.com>
2024-01-30 03:33:03 +0000

    Update copyright notice on certain source files

--- ffuzzy/src/generate_easy.rs
+++ ffuzzy/src/generate_easy.rs
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-// SPDX-FileCopyrightText: Copyright (C) 2023 Tsukasa OI <floss_ssdeep@irq.a4lg.com>.
+// SPDX-FileCopyrightText: Copyright (C) 2023, 2024 Tsukasa OI <floss_ssdeep@irq.a4lg.com>.
 
 #![cfg(feature = "easy-functions")]
 
