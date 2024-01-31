96dfea7d4fd5b8357c856f205fcada8a22b98d9c
Tsukasa OI <floss_ssdeep@irq.a4lg.com>
2024-01-31 04:35:07 +0000

    tests: hash: Remove complex sequence tests
    It is now proven that complex sequence tests does not improve any coverage.
    Since it takes huge amount of time, removing it improves the test time
    when the feature `tests-very-slow` is enabled (this commit makes "very slow"
    tests empty but anyway).

--- ffuzzy/src/hash/test_utils.rs
+++ ffuzzy/src/hash/test_utils.rs
@@ -5,17 +5,6 @@
 use crate::hash::block::block_hash;
 
 
-const NUM_SLOW_SEQUENCES: usize = 4;
-
-#[test]
-fn test_common_prerequisites() {
-    // Generic requirements
-    assert!(block_hash::ALPHABET_SIZE <= block_hash::FULL_SIZE);
-    // test_blockhash_content_multiple_sequences
-    assert!(block_hash::ALPHABET_SIZE > NUM_SLOW_SEQUENCES);
-}
-
-
 fn test_blockhash_content_no_sequences(variant: bool, test_func: &mut impl FnMut(&[u8], &[u8])) {
     for len in 0..=block_hash::FULL_SIZE {
         let mut bh = [0u8; block_hash::FULL_SIZE];
@@ -93,64 +82,12 @@ fn test_blockhash_contents_division(max_bh2: usize, test_func: &mut impl FnMut(&
     });
 }
 
-#[cfg(feature = "tests-very-slow")]
-fn test_blockhash_content_multiple_sequences(test_func: &mut impl FnMut(&[u8], &[u8])) {
-    // Generated sequences of block hash:
-    // "BCDE", "BCDEE",... "BCDDE", "BCDDEE",... "BBB...BCDE"
-    // ("B" * l1 :: "C" * l2 :: "D" * l3 :: "E" * l4 for l1..l4 > 0 and sum(l1..l4) <= FULL_SIZE)
-    const NUM_SEQUENCES: usize = NUM_SLOW_SEQUENCES;
-    for l1 in 1..=(block_hash::FULL_SIZE - (NUM_SEQUENCES - 1)) {
-        let s1 = usize::min(l1, block_hash::MAX_SEQUENCE_SIZE);
-        let total = l1;
-        for l2 in 1..=(block_hash::FULL_SIZE - total - (NUM_SEQUENCES - 2)) {
-            let s2 = usize::min(l2, block_hash::MAX_SEQUENCE_SIZE);
-            let total = total + l2;
-            for l3 in 1..=(block_hash::FULL_SIZE - total - (NUM_SEQUENCES - 3)) {
-                let s3 = usize::min(l3, block_hash::MAX_SEQUENCE_SIZE);
-                let total = total + l3;
-                for l4 in 1..=(block_hash::FULL_SIZE - total - (NUM_SEQUENCES - 4)) {
-                    let s4 = usize::min(l4, block_hash::MAX_SEQUENCE_SIZE);
-                    let total = total + l4;
-                    // Make raw four sequences
-                    let mut seq = [0u8; block_hash::FULL_SIZE];
-                    seq[0..l1].fill(1);
-                    seq[l1..l1+l2].fill(2);
-                    seq[l1+l2..l1+l2+l3].fill(3);
-                    seq[l1+l2+l3..l1+l2+l3+l4].fill(4);
-                    // Make normalized four sequences
-                    let mut seq_norm = [0u8; block_hash::FULL_SIZE];
-                    let total_norm = s1 + s2 + s3 + s4;
-                    seq_norm[0..s1].fill(1);
-                    seq_norm[s1..s1+s2].fill(2);
-                    seq_norm[s1+s2..s1+s2+s3].fill(3);
-                    seq_norm[s1+s2+s3..s1+s2+s3+s4].fill(4);
-                    test_func(&seq[0..total], &seq_norm[0..total_norm]);
-                }
-            }
-        }
-    }
-}
-
-#[cfg(feature = "tests-very-slow")]
-fn test_blockhash_contents_multiple_sequences(test_func: &mut impl FnMut(&[u8], &[u8], &[u8], &[u8])) {
-    // Because arbitrary division takes some time, we use single loop
-    // (unlike double loop on others)
-    test_blockhash_content_multiple_sequences(&mut |bh, bh_norm| {
-        test_func(bh, &[], bh_norm, &[]);
-        test_func(&[], bh, &[], bh_norm);
-    });
-}
-
 
 pub(crate) fn test_blockhash_contents_all(test_func: &mut impl FnMut(&[u8], &[u8], &[u8], &[u8])) {
     test_blockhash_contents_no_sequences(test_func);
     test_blockhash_contents_one_sequence(test_func);
     test_blockhash_contents_division(block_hash::FULL_SIZE, test_func);
     test_blockhash_contents_division(block_hash::HALF_SIZE, test_func);
-    #[cfg(feature = "tests-very-slow")]
-    {
-        test_blockhash_contents_multiple_sequences(test_func);
-    }
 }
 
 pub(crate) fn test_blockhash_content_all(test_func: &mut impl FnMut(&[u8], &[u8])) {
@@ -158,8 +95,4 @@ pub(crate) fn test_blockhash_content_all(test_func: &mut impl FnMut(&[u8], &[u8]
     test_blockhash_content_one_sequence(1u8, test_func);
     test_blockhash_content_division(block_hash::FULL_SIZE, false, test_func);
     test_blockhash_content_division(block_hash::HALF_SIZE, false, test_func);
-    #[cfg(feature = "tests-very-slow")]
-    {
-        test_blockhash_content_multiple_sequences(test_func);
-    }
 }
