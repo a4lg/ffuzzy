15c6bd06ddb53210a5d51f2ed092332c475b4221
Tsukasa OI <floss_ssdeep@irq.a4lg.com>
2024-03-28 07:46:10 +0000

    doc: Minor formatting
    *   Add module-level summary.
    *   Format `true`.

--- ffuzzy/src/intrinsics.rs
+++ ffuzzy/src/intrinsics.rs
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: CC0-1.0
 // SPDX-FileCopyrightText: Authored by Tsukasa OI <floss_ssdeep@irq.a4lg.com> in 2023, 2024
 
-/// Hints to the compiler that branch condition is likely to be true.
+//! Compiler intrinsics to control optimization of this crate.
+
+/// Hints to the compiler that branch condition is likely to be [`true`].
 ///
 /// This is a thin wrapper to [`core::intrinsics::likely()`] and requires
 /// `#![feature(core_intrinsics)]` when the `unstable` feature is enabled.
@@ -17,7 +19,7 @@ pub(crate) fn likely(value_likely_to_be_true: bool) -> bool {
     }
 }
 
-/// Hints to the compiler that branch condition is unlikely to be true.
+/// Hints to the compiler that branch condition is unlikely to be [`true`].
 ///
 /// This is a thin wrapper to [`core::intrinsics::unlikely()`] and requires
 /// `#![feature(core_intrinsics)]` when the `unstable` feature is enabled.
