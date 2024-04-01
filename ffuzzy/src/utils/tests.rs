e4408212172d40835b3d372891e8b4e4095f3ee5
Tsukasa OI <floss_ssdeep@irq.a4lg.com>
2024-04-01 07:57:55 +0000

    utils: Add test module description (though invisible)

--- ffuzzy/src/utils/tests.rs
+++ ffuzzy/src/utils/tests.rs
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 // SPDX-FileCopyrightText: Copyright (C) 2023, 2024 Tsukasa OI <floss_ssdeep@irq.a4lg.com>.
 
+//! Tests: [`crate::utils`].
+
 #![cfg(test)]
 
 use super::{u64_ilog2, u64_lsb_ones};
