80d1ec77d4a1879fddf5666ad766362b7fe76ecb
Tsukasa OI <floss_ssdeep@irq.a4lg.com>
2024-04-01 07:30:26 +0000

    base64, test_utils: Reformat "copyright" notice
    Replace "2023 and 2024" with "2023, 2024" for consistency.

--- ffuzzy/src/base64.rs
+++ ffuzzy/src/base64.rs
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: CC0-1.0
-// SPDX-FileCopyrightText: Authored by Tsukasa OI <floss_ssdeep@irq.a4lg.com> in 2023 and 2024
+// SPDX-FileCopyrightText: Authored by Tsukasa OI <floss_ssdeep@irq.a4lg.com> in 2023, 2024
 
 //! Base64 handlings.
 
