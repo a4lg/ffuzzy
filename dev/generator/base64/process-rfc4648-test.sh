8807e04a8dadb9c694946102a42da2cfdad53f27
Tsukasa OI <floss_ssdeep@irq.a4lg.com>
2025-01-21 07:14:19 +0000

    base64: Simplify alphabet-index test
    By moving certain checks to the test case generator,
    we shorten and simplify the test case.

--- dev/generator/base64/process-rfc4648-test.sh
+++ dev/generator/base64/process-rfc4648-test.sh
@@ -1,14 +1,33 @@
 #! /bin/sh
 # SPDX-License-Identifier: MIT
-# SPDX-FileCopyrightText: Copyright (C) 2023, 2024 Tsukasa OI <floss_ssdeep@irq.a4lg.com>.
-if test ! -f rfc4648.txt
-then
+# SPDX-FileCopyrightText: Copyright (C) 2023–2025 Tsukasa OI <floss_ssdeep@irq.a4lg.com>.
+if test ! -f rfc4648.txt; then
     echo "ERROR: rfc4648.txt not found." 1>&2
     echo "You may download RFC 4648 text file (rfc4648.txt) by for instance:" 1>&2
     echo "                   wget https://www.rfc-editor.org/rfc/rfc4648.txt" 1>&2
     exit 1
 fi
-cat rfc4648.txt \
-    | awk -f process-rfc4648-test-1.awk \
-    | sort -n -k 1 \
-    | awk '{printf("    assert_base64!(%d, b'"'"'%s'"'"');\n", $1, $2);}'
+PREV_N=-1
+printf '%s\n%s' '    assert_base64_cases! {' '        ['
+cat rfc4648.txt |
+    awk -f process-rfc4648-test-1.awk |
+    sort -n -k 1,1 |
+    while read N CH; do
+        # Check that all alphabet values are continuous from 0
+        # (starts from 0 and continue like 1, 2, 3...)
+        if test $(expr $PREV_N + 1) -ne $N; then
+            printf '@ERROR@'
+            break
+        fi
+        # New line if necessary.
+        case "$CH" in
+        [AOao0+])
+            printf '\n           '
+            ;;
+        esac
+        # Print test case.
+        printf " b'%s'," "$CH"
+        # Prepare for the next alphabet.
+        PREV_N=$N
+    done
+printf '\n%s\n%s\n' '        ]' '    }'
--- ffuzzy/src/base64/tests.rs
+++ ffuzzy/src/base64/tests.rs
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-// SPDX-FileCopyrightText: Copyright (C) 2024 Tsukasa OI <floss_ssdeep@irq.a4lg.com>.
+// SPDX-FileCopyrightText: Copyright (C) 2024–2025 Tsukasa OI <floss_ssdeep@irq.a4lg.com>.
 
 //! Tests: [`crate::base64`].
 
@@ -9,84 +9,29 @@ use super::{base64_index, base64_index_simple, BASE64_INVALID, BASE64_TABLE_U8};
 
 #[test]
 fn values_and_indices() {
-    let mut covered_idxes = 0u64;
-    let mut expected_idx = 0;
-    let mut assert_base64 = |idx: usize, ch| {
-        // Test indices sequentially (0..=63).
-        assert!(idx < 64);
-        assert_eq!(expected_idx, idx);
-        assert_eq!(base64_index_simple(ch), Some(idx as u8));
-        assert_eq!(BASE64_TABLE_U8[idx], ch);
-        covered_idxes |= 1 << idx;
-        expected_idx += 1;
-    };
-    assert_base64(0, b'A');
-    assert_base64(1, b'B');
-    assert_base64(2, b'C');
-    assert_base64(3, b'D');
-    assert_base64(4, b'E');
-    assert_base64(5, b'F');
-    assert_base64(6, b'G');
-    assert_base64(7, b'H');
-    assert_base64(8, b'I');
-    assert_base64(9, b'J');
-    assert_base64(10, b'K');
-    assert_base64(11, b'L');
-    assert_base64(12, b'M');
-    assert_base64(13, b'N');
-    assert_base64(14, b'O');
-    assert_base64(15, b'P');
-    assert_base64(16, b'Q');
-    assert_base64(17, b'R');
-    assert_base64(18, b'S');
-    assert_base64(19, b'T');
-    assert_base64(20, b'U');
-    assert_base64(21, b'V');
-    assert_base64(22, b'W');
-    assert_base64(23, b'X');
-    assert_base64(24, b'Y');
-    assert_base64(25, b'Z');
-    assert_base64(26, b'a');
-    assert_base64(27, b'b');
-    assert_base64(28, b'c');
-    assert_base64(29, b'd');
-    assert_base64(30, b'e');
-    assert_base64(31, b'f');
-    assert_base64(32, b'g');
-    assert_base64(33, b'h');
-    assert_base64(34, b'i');
-    assert_base64(35, b'j');
-    assert_base64(36, b'k');
-    assert_base64(37, b'l');
-    assert_base64(38, b'm');
-    assert_base64(39, b'n');
-    assert_base64(40, b'o');
-    assert_base64(41, b'p');
-    assert_base64(42, b'q');
-    assert_base64(43, b'r');
-    assert_base64(44, b's');
-    assert_base64(45, b't');
-    assert_base64(46, b'u');
-    assert_base64(47, b'v');
-    assert_base64(48, b'w');
-    assert_base64(49, b'x');
-    assert_base64(50, b'y');
-    assert_base64(51, b'z');
-    assert_base64(52, b'0');
-    assert_base64(53, b'1');
-    assert_base64(54, b'2');
-    assert_base64(55, b'3');
-    assert_base64(56, b'4');
-    assert_base64(57, b'5');
-    assert_base64(58, b'6');
-    assert_base64(59, b'7');
-    assert_base64(60, b'8');
-    assert_base64(61, b'9');
-    assert_base64(62, b'+');
-    assert_base64(63, b'/');
-    // Make sure that all 64 alphabets are covered.
-    assert_eq!(covered_idxes, u64::MAX);
-    assert_eq!(expected_idx, 64);
+    macro_rules! assert_base64_cases {
+        {[ $($ch:expr),* $(,)? ]} => {
+            let mut idx = 0usize;
+            $(
+                assert!(idx < 64);
+                assert_eq!(base64_index_simple($ch), Some(idx as u8));
+                assert_eq!(BASE64_TABLE_U8[idx], $ch);
+                idx += 1;
+            )*
+            // Make sure that all 64 alphabets are covered.
+            assert_eq!(idx, 64);
+        }
+    }
+    assert_base64_cases! {
+        [
+            b'A', b'B', b'C', b'D', b'E', b'F', b'G', b'H', b'I', b'J', b'K', b'L', b'M', b'N',
+            b'O', b'P', b'Q', b'R', b'S', b'T', b'U', b'V', b'W', b'X', b'Y', b'Z',
+            b'a', b'b', b'c', b'd', b'e', b'f', b'g', b'h', b'i', b'j', b'k', b'l', b'm', b'n',
+            b'o', b'p', b'q', b'r', b's', b't', b'u', b'v', b'w', b'x', b'y', b'z',
+            b'0', b'1', b'2', b'3', b'4', b'5', b'6', b'7', b'8', b'9',
+            b'+', b'/',
+        ]
+    }
 }
 
 #[test]
