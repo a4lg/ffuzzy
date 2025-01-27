7015d4737b4fbff77eb9f92c96cc41be1315498c
Tsukasa OI <floss_ssdeep@irq.a4lg.com>
2025-01-27 04:41:31 +0000

    hash-blocksize-debruijn: Make variable name semantic

--- dev/generator/hash-blocksize-debruijn/src/main.rs
+++ dev/generator/hash-blocksize-debruijn/src/main.rs
@@ -7,12 +7,12 @@ use std::process;
 
 fn main() {
     const MULTIPLIER: u32 = 3;
-    const BITS_TO_COVER: u32 = MULTIPLIER.leading_zeros() + 1;
+    const NUM_VALID: u32 = MULTIPLIER.leading_zeros() + 1;
     const SHIFT_WIDTH: u32 = u32::BITS.leading_zeros() + 1;
 
     // Parameter constraints for ssdeep
     assert!(MULTIPLIER == 3);
-    assert!(BITS_TO_COVER == 31);
+    assert!(NUM_VALID == 31);
     assert!(SHIFT_WIDTH == 27);
 
     // Search a variant of de Bruijn sequence
@@ -21,12 +21,12 @@ fn main() {
     }
     for debruijn_mul in ((1u32 << (SHIFT_WIDTH - 1)) / MULTIPLIER)..=u32::MAX {
         let mut table_fill: u32 = 0;
-        for i in 0..BITS_TO_COVER {
+        for i in 0..NUM_VALID {
             table_fill |= 1u32 << debruijn_index(i, debruijn_mul);
         }
-        if table_fill.count_ones() == BITS_TO_COVER {
+        if table_fill.count_ones() == NUM_VALID {
             let mut table: [u8; 32] = [0xff; 32];
-            for i in 0..BITS_TO_COVER {
+            for i in 0..NUM_VALID {
                 table[debruijn_index(i, debruijn_mul) as usize] = i as u8;
             }
             println!("const LOG_DEBRUIJN_CONSTANT: u32 = 0x{:08x};", debruijn_mul);
