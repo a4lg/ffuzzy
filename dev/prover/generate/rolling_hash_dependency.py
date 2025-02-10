4961e97ce1894166533493d2a18b962dc5e2f59f
Tsukasa OI <floss_ssdeep@irq.a4lg.com>
2025-02-10 12:50:19 +0000

    dev: Modernize `rolling_hash_dependency.py`

--- dev/prover/generate/rolling_hash_dependency.py
+++ dev/prover/generate/rolling_hash_dependency.py
@@ -1,17 +1,19 @@
 #! /bin/env python3
 # SPDX-License-Identifier: MIT
-# SPDX-FileCopyrightText: Copyright (C) 2024 Tsukasa OI <floss_ssdeep@irq.a4lg.com>.
+# SPDX-FileCopyrightText: Copyright (C) 2024–2025 Tsukasa OI <floss_ssdeep@irq.a4lg.com>.
 
 import sys
 import z3
+
 
 def DeMorganNot(and_clauses):
     # Convert AND clauses and get the complement expression
     # (e.g. for X && Y [and_clauses], return !(X && Y) == (!X || !Y))
     return z3.Or(*[z3.Not(p) for p in and_clauses])
 
+
 def FindCounterexamples(name, constraints):
-    print('Whether {} has a counterexample... '.format(name), file=sys.stderr, end='')
+    print(f'Whether {name} has a counterexample... ', file=sys.stderr, end='')
     sys.stderr.flush()
     solver = z3.Solver()
     for constraint in constraints:
@@ -21,11 +23,10 @@ def FindCounterexamples(name, constraints):
         print('found!\n\nCounterexample:', file=sys.stderr)
         model = solver.model()
         for d in sorted(model.decls(), key=str):
-            print('{} = {}'.format(d, model[d]))
+            print(f'\t{d} = {model[d]}')
         sys.exit(1)
     else:
         print('not found.', file=sys.stderr)
-
 
 
 # Rolling hash parameters
@@ -33,15 +34,17 @@ BITS_OF_WORD = 32
 BITS_OF_BYTE = 8
 ROLLING_WINDOW = 7
 H3_LSHIFT = 5
-assert(BITS_OF_BYTE <= BITS_OF_WORD)
+assert BITS_OF_BYTE <= BITS_OF_WORD
+
+
 class RollingHash:
     def __init__(self, name):
         self.n_bytes = 0
-        self.h1 = z3.BitVec('{}_h1'.format(name), BITS_OF_WORD)
-        self.h2 = z3.BitVec('{}_h2'.format(name), BITS_OF_WORD)
-        self.h3 = z3.BitVec('{}_h3'.format(name), BITS_OF_WORD)
+        self.h1 = z3.BitVec(f'{name}_h1', BITS_OF_WORD)
+        self.h2 = z3.BitVec(f'{name}_h2', BITS_OF_WORD)
+        self.h3 = z3.BitVec(f'{name}_h3', BITS_OF_WORD)
         self.window = [
-            z3.BitVec('{}_wi_{}'.format(name, i), BITS_OF_BYTE) for i in range(ROLLING_WINDOW)
+            z3.BitVec(f'{name}_wi_{i}', BITS_OF_BYTE) for i in range(ROLLING_WINDOW)
         ]
         self.constraints = [
             self.h1 == 0,
@@ -51,6 +54,7 @@ class RollingHash:
         self.constraints += [
             self.window[i] == 0 for i in range(ROLLING_WINDOW)
         ]
+
     def update(self, ch):
         ch_word = z3.ZeroExt(BITS_OF_WORD - BITS_OF_BYTE, ch)
         fading_byte = self.window[0]
@@ -67,10 +71,10 @@ class RollingHash:
 
 # Old (fading) bytes (as prefix)
 # Note: they may be different (there's no constraints for them)
-old_1 = [z3.BitVec('o1_{}'.format(i), BITS_OF_BYTE) for i in range(ROLLING_WINDOW)]
-old_2 = [z3.BitVec('o2_{}'.format(i), BITS_OF_BYTE) for i in range(ROLLING_WINDOW)]
+old_1 = [z3.BitVec(f'o1_{i}', BITS_OF_BYTE) for i in range(ROLLING_WINDOW)]
+old_2 = [z3.BitVec(f'o2_{i}', BITS_OF_BYTE) for i in range(ROLLING_WINDOW)]
 # New updating bytes
-new_b = [z3.BitVec('n__{}'.format(i), BITS_OF_BYTE) for i in range(ROLLING_WINDOW)]
+new_b = [z3.BitVec(f'n__{i}', BITS_OF_BYTE) for i in range(ROLLING_WINDOW)]
 
 for initial_old in range(0, ROLLING_WINDOW + 1):
     r1 = RollingHash('r1')
@@ -84,8 +88,8 @@ for initial_old in range(0, ROLLING_WINDOW + 1):
     # Make sure that the rolling hash values only depend on
     # the last ROLLING_WINDOW bytes.
     FindCounterexamples(
-        "Rolling hash dependency (prefix: {})".format(initial_old),
-        r1.constraints + r2.constraints + \
+        f'rolling hash dependency (prefix: {initial_old})',
+        r1.constraints + r2.constraints +
         [DeMorganNot([
             r1.h1 == r2.h1,
             r1.h2 == r2.h2,
@@ -93,5 +97,5 @@ for initial_old in range(0, ROLLING_WINDOW + 1):
         ])]
     )
     # Make sure that the window is completely overwritten by new updating bytes.
-    assert(r1.window == new_b)
-    assert(r2.window == new_b)
+    assert r1.window == new_b
+    assert r2.window == new_b
